eb46dbb696310278f877e028915e7086c0bc3c02
kenny11cz <dan.balarin@gmail.com>
2014-10-17 15:03:52 +0200

    Konec komplexnich cisel xD

--- KomplexniCisla/main.py
+++ KomplexniCisla/main.py
@@ -2,5 +2,8 @@ from trida import Complex
 
 
 cislo1 = Complex(1,3)
-cislo2 = Complex(1,-1)
+cislo2 = Complex(1,2)
 print(cislo1+cislo2)
+print(cislo1.velikost())
+print(cislo2.velikost())
+print(cislo1>cislo2)
--- KomplexniCisla/trida.py
+++ KomplexniCisla/trida.py
@@ -1,10 +1,11 @@
 import math
+
 class Complex():
     def __init__(self, cela_slozka = 0, imaginarni_slozka = 0):
         self.real = cela_slozka
         self.imag = imaginarni_slozka
     def velikost(self):
-        vector = math.sqrt((self.real*self.real)+(self.imag*self.imag))
+        vector = math.sqrt((self.real**2)+(self.imag**2))
         return vector
     def sdruzene(self):
         return Complex(self.real, -self.imag)
@@ -18,3 +19,16 @@ class Complex():
         return result
     def __add__(self, other):
         return Complex(self.real + other.real, self.imag + other.imag)
+    def __sub__(self, other):
+        return Complex(self.real - other.real, self.imag - other.imag)
+    def __eq__(self, other):
+        vysledek = (self.imag == other.imag) and (self.imag == other.imag)
+        return vysledek
+    def __gt__(self, other):
+        return self.velikost() > other.velikost()
+    def __lt__(self, other):
+        return self.velikost() < other.velikost()
+    def __del__(self):
+        print("Good bye fellaz")
+
+
